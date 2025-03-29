930a2622ee3c7e01ad36d0f009950b676af47913
chrishayuk <chrishayuk@googlemail.com>
2025-03-29 23:54:19 +0000

    missed a pydantic import

--- src/chuk_mcp/mcp_client/transport/stdio/stdio_server_parameters.py
+++ src/chuk_mcp/mcp_client/transport/stdio/stdio_server_parameters.py
@@ -1,8 +1,8 @@
 # chuk_mcp/mcp_client/transport/stdio/stdio_server_parameters.py
-from pydantic import BaseModel, Field
 from typing import Any, Dict, Optional
+from chuk_mcp.mcp_client.mcp_pydantic_base import McpPydanticBase, Field
 
-class StdioServerParameters(BaseModel):
+class StdioServerParameters(McpPydanticBase):
     command: str
     args: list[str] = Field(default_factory=list)
     env: Optional[Dict[str, str]] = None
